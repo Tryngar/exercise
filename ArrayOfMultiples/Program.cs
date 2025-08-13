a99a4af01d2be1143248323297f838a54830a40f
Sascha Hnilicka <saschahnilicka@gmail.com>
2025-08-13 14:07:03 +0200

    first exercise

---
+++ ArrayOfMultiples/Program.cs
@@ -0,0 +1,28 @@
+﻿
+// https://edabit.com/challenge/2QvnWexKoLfcJkSsc
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] x = ArrayOfMultiples(7, 5);
+        int[] y = ArrayOfMultiples(12, 10);
+        int[] z = ArrayOfMultiples(17, 6);
+
+        foreach (int i in z)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    public static int[] ArrayOfMultiples(int num, int length)
+    {
+        int[] result = new int[length];
+
+        for (int i = 1; i <= length; i++)
+        {
+            result[i - 1] = num * i;
+        }
+        return result;
+    }
+}
